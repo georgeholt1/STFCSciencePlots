1355219a173def10f127f24476da51a6b663ade4
George Holt <george.holt@stfc.ac.uk>
2022-10-17 16:42:22 +0100

    Adding examples

---
+++ examples/plot_examples.py
@@ -0,0 +1,31 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+from stfcscienceplots import legend as stfclegend
+
+plt.style.use('stfc')
+
+# Data to plot and range
+def sigmoid(x, w):
+    return 1 / (1 + np.e ** (-w * x))
+
+x = np.linspace(-5, 5, 100)
+vals = (0.3, 0.5, 0.9, 1.5, 3)
+
+# Plotting
+fig, ax = plt.subplots()
+for val in vals:
+    ax.plot(x, sigmoid(x, val), label=f'${{{val}}}$')
+
+ax.set_xlim(x.min(), x.max())
+ax.set_xlabel('$x$')
+ax.set_ylabel(r'$\frac{1}{1 + \mathrm{e}^{-wx}}$')
+
+stfclegend(ax, legend_kwargs={'title': '$w$'})
+
+# Saving
+outdir = os.path.join('.', 'img')
+if not os.path.isdir(outdir):
+    os.makedirs(outdir)
+fig.savefig(os.path.join(outdir, 'plot.png'))
